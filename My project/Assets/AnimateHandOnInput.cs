bef3948afd45fe82805a3df87e4685455c928503
unknown <jeremy.clay@selu.edu>
2022-11-28 22:17:53 -0600

    Fixed Error handAnimator

--- My project/Assets/AnimateHandOnInput.cs
+++ My project/Assets/AnimateHandOnInput.cs
@@ -7,7 +7,7 @@ using UnityEngine.InputSystem;
 public class AnimateHandOnInput : MonoBehaviour
 {
 
-    public InputActionProperty pinchAnimationAction;
+   public InputActionProperty pinchAnimationAction;
     public InputActionProperty gripAnimationAction;
     public Animator  handAnimator;
 
